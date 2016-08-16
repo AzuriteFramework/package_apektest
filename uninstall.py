25d45a364285f648d55d9fd676be30865ce6d73c
Maximilian H <redc0der.mail@gmail.com>
2016-08-16 22:41:01 +0200

    Update uninstall.py

--- uninstall.py
+++ uninstall.py
@@ -1,8 +1,8 @@
-import os
+#import os
 
-print str(os.system('mkdir apektest'))
-print str(os.system('mkdir tmp/foo'))
-print str(os.system('touch bar.txt'))
+#print str(os.system('mkdir apektest'))
+#print str(os.system('mkdir tmp/foo'))
+#print str(os.system('touch bar.txt'))
 
-print str(os.system('ls'))
+#print str(os.system('ls'))
 
